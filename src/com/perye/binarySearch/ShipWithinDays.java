938263466a17edcddb684b45de9d9d1157e72d08
honggd <11143526@qq.com>
2021-04-28 18:10:27 +0800

    update

---
+++ src/com/perye/binarySearch/ShipWithinDays.java
@@ -0,0 +1,38 @@
+package com.perye.binarySearch;
+
+import java.util.Arrays;
+
+/**
+ * 1011. 在 D 天内送达包裹的能力
+ * 数组、二分查找
+ * 链接：https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days/
+ *
+ * @author honggd
+ */
+public class ShipWithinDays {
+
+    public int shipWithinDays(int[] weights, int D) {
+        // 确定二分查找左右边界
+        int left = Arrays.stream(weights).max().getAsInt(), right = Arrays.stream(weights).sum();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            // need 为需要运送的天数
+            // cur 为当前这一天已经运送的包裹重量之和
+            int need = 1, cur = 0;
+            for (int weight : weights) {
+                if (cur + weight > mid) {
+                    ++need;
+                    cur = 0;
+                }
+                cur += weight;
+            }
+            if (need <= D) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+}
---
+++ src/com/perye/tree/RangeSumBST.java
@@ -0,0 +1,46 @@
+package com.perye.tree;
+
+/**
+ * 938. 二叉搜索树的范围和
+ * 树、深度优先搜索
+ * 链接：https://leetcode-cn.com/problems/range-sum-of-bst/
+ *
+ * @author honggd
+ */
+public class RangeSumBST {
+
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}

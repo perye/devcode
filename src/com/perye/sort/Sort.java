3b5289d1a450a565e34da09d482befdd94092eb0
honggd <11143526@qq.com>
2021-05-14 18:00:39 +0800

    update

---
+++ src/com/perye/sort/Sort.java
@@ -0,0 +1,57 @@
+package com.perye.sort;
+
+/**
+ * 排序
+ *
+ * @author honggd
+ */
+public class Sort {
+
+
+    /**
+     * 交换元素-引入第三个中间变量
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 交换元素-加法(可能溢出)
+     */
+    private static void swapByAdd(int[] arr, int i, int j) {
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    /**
+     * 交换元素-异或(防止溢出)
+     */
+    private static void swapByXor(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+
+    /**
+     * 冒泡排序
+     *
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // 如果左边的数大于右边的数，则交换，保证右边的数字最大
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+
+
+}

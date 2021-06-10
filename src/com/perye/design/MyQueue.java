188b2e4dc31e1e11e5aa441251cdd260e5986e1f
honggd <11143526@qq.com>
2021-06-10 14:05:49 +0800

    update

---
+++ src/com/perye/design/MyQueue.java
@@ -0,0 +1,78 @@
+package com.perye.design;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 232. 用栈实现队列
+ * <p>
+ * 将一个栈当作输入栈，用于压入 push 传入的数据；
+ * 另一个栈当作输出栈，用于 pop 和 peek 操作。
+ * 每次 pop 或 peek 时，若输出栈为空则将输入栈的全部数据依次弹出并压入输出栈，这样输出栈从栈顶往栈底的顺序就是队列从队首往队尾的顺序
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ *
+ * @author perye
+ */
+public class MyQueue {
+
+    Deque<Integer> inStack;
+    Deque<Integer> outStack;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        inStack = new ArrayDeque<>();
+        outStack = new ArrayDeque<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
---
+++ src/com/perye/dp/UniquePathsWithObstacles.java
@@ -0,0 +1,33 @@
+package com.perye.dp;
+
+/**
+ * 63. 不同路径 II
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/unique-paths-ii/
+ *
+ * @author perye
+ */
+public class UniquePathsWithObstacles {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    if (i > 0 && j > 0) {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    } else if (i > 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else if (j > 0) {
+                        dp[i][j] = dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+}
